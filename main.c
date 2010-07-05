c309e09c97bc38c9dc1e1fa998bf1b2cf96070dd
Wim Looman <ghostunderscore@gmail.com>
2010-07-05 17:59:34 +1200

    Added license information to the files.

---
+++ main.c
@@ -0,0 +1,11 @@
+/*
+ * Authors:
+ *      Wim Looman
+ *      Henry Jenkins
+ *      Kate Markman
+ *      Ben Snalam
+ * Copyright:
+ *      Copyright (c) 2010 Wim Looman, Henry Jenkins, Kate Markman, Ben Snalam
+ * License:
+ *      GNU General Public License (see LICENSE in root folder)
+ */
