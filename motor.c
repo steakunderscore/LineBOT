49338d4c0c58749706d62561a615463d7a6659ef
Henry Jenkins <henry@henryjenkins.name>
2010-07-12 18:14:29 +1200

    Fixed the PWM after reading through the pdf

--- motor.c
+++ motor.c
@@ -18,42 +18,57 @@
 void m_initialize( void )
 {
     /*
-       TCCR1A - Timer Counter Control Register (TIMER1A)
+       TCCR1 - Timer Counter Control Register (TIMER1)
        -----------------------------------------------
        BITS DESCRIPTION
 
-
+       TCCR1A
        NO:   NAME   DESCRIPTION
        --------------------------
-       BIT 7 : FOC0   Force Output Compare [Not used in this]
-       BIT 6 : WGM00  Wave form generation mode [SET to 1]
-       BIT 5 : COM01  Compare Output Mode        [SET to 1]
-       BIT 4 : COM00  Compare Output Mode        [SET to 0]
+       BIT 0 : WGM10 [SET to 1]
+       BIT 1 : WGM11 [SET to 0]
+       BIT 2 : Don't care
+       BIT 3 : Don't care
+       BIT 4 : COM1B0 Invert selection           [SET to 0]
+       BIT 5 : COM1B1 Enable OC1B PWM            [SET to 1]
+       BIT 6 : COM1A0 Invert selection           [SET to 0]
+       BIT 7 : COM1A1 Enable OC1A PWM            [SET to 1]
        
-       BIT 3 : WGM01  Wave form generation mode [SET to 1]
-       BIT 2 : CS02   Clock Select               [SET to 0]
-       BIT 1 : CS01   Clock Select               [SET to 1]
-       BIT 0 : CS00   Clock Select               [SET to 1]
+       TCCR1B
+       NO:   NAME   DESCRIPTION
+       --------------------------
+       BIT 0 : CS10 [SET to 0]
+       BIT 1 : CS11 [SET to 1]
+       BIT 2 : CS12 [SET to 0]
+       BIT 3 : WGM12 [SET to 1]
+       BIT 4 : WGM13 [SET to 0]
+       Rest of bits we don't care about
+       
        
        The above settings are for
        --------------------------
        
-       Timer Clock = CPU Clock/64
+       Timer Clock = CPU Clock/8
        Mode        = Fast PWM
        PWM Output  = Non Inverted
+       Outputs     = OC1A and OC1B
 
      */
 
-
-    TCCR1A|=(1<<WGM00)|(1<<WGM01)|(1<<COM01)|(1<<CS00)|(1<<CS01);
+    TCCR1A = BIT(WGM10) | BIT(COM1B1) | BIT(COM1A1);
+    TCCR1B = BIT(CS11) | BIT(WGM12);
 
     // Set OC1A and OC1B pins as the outputs.
 
+    //TODO: Im unsure about this line
     DDRB|=(1<<PB1)|(1<<PB2);
 
     //Set the output to off.
     OC1A=0;
     OC1B=0;
+
+    // Zero the counter
+    TCNT1 = 0;
 }
 
 void m_turn(unsigned char direction) {
