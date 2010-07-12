c260abbc19fb01b634dc9c5cb131a3c51b7c3d71
Wim Looman <ghostunderscore@gmail.com>
2010-07-12 18:08:44 +1200

    Added more stubs for the motor functions.

--- motor.h
+++ motor.h
@@ -12,5 +12,26 @@
 
 #include "constants.h"
 
-// Starts the robot turning in the specified direction.
+// Starts the robot turning in the specified direction, this keeps the
+// direction wheel still and drives the other wheel.
 void m_turn(unsigned char direction);
+
+// Starts the robot rotating in the specified direction, this drives the
+// direction wheel in reverse and the other wheel fowards.
+void m_rotate(unsigned char direction);
+
+// Starts the robot moving forwards.
+void m_forwards();
+
+// Moves the robot forward approximately distance mm.
+void m_forwards(unsigned char distance);
+
+// Starts the robot moving backwards.
+void m_reverse();
+
+// Moves the robot backwards approximately distance mm.
+void m_reverse(unsigned char distance);
+
+// Stops the robot.
+void m_stop();
+
