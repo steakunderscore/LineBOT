d30f26ef2831df22f53bcc9cb321db7f168acbe5
Ben Snalam <snailman8@gmail.com>
2010-07-12 16:53:44 +1200

    first working source of sensor

--- sensor.c
+++ sensor.c
@@ -11,8 +11,82 @@
  */
 
 #include "sensor.h"
+#define GREY_THRESH 128     // NOTE: these are currently might as well be random numbers and assume that complete blackness = 0, white = 256
+#define WHITE_THRESH 192
+
+/**
+ADCsRA
+ADEN 7 enables ADC
+ADSC 6 = start conversion
+ADFR 5 = free runnning (dont want)
+ADIF 4 = interupt flag (1 if complete) (dont want)
+ADIE 3 = interupt enabler, this along with SREG I-bit enable ADC Interupt (dont want)
+00000111 = prescalar selections, initial try no scaling
+
+ADMUX
+will try 
+REFS1 7 = 0 ,
+REFS0 6 = 0 (Vref is AREF Internal Vref off)
+
+ADLAR 5 = 1 (left adjust) (want)
+bit4 - 0
+MUX3 3 = input selections
+MUX2 2 = input selections
+MUX1 1 = input selections
+MUX0 0 = input selections
+
+MUX3->0 :
+0000 = ADC0
+0001 = ADC1
+0010 = ADC2
+0011 = ADC3
+0100 = ADC4
+0101 = ADC5
+0110 = ADC6
+0111 = ADC7 - check to see which pins using.'
+*/
 unsigned char s_check(unsigned char sensor)
 {
-    while(1)
-        break;
+    uint8_t result= 0;
+                            //check addresses are correct
+    if(sensor == LEFT) 
+    {
+        ADMUX = (BIT(ADLAR) | BIT(MUX1));
+    }
+    
+    else if(sensor == RIGHT)
+    {
+        ADMUX = BIT(ADLAR); //set for right sensor ADLAR = 1
+    }
+    
+    else if(sensor == CENTER)
+    {
+        ADMUX = (BIT(ADLAR)|BIT(MUX0));
+    }
+    
+    ADCSRA = ADCSRA | BIT(ADSC); // starts ADC
+    
+    while((ADCSRA & BIT(ADSC)) & BIT(ADSC)) //waits for ADC CONVERTION
+    {
+        continue;
+    }
+    
+    
+    if(ADCH < GREY_THRESH)
+    {    
+        result = S_BLACK;
+    }
+    else if(ADCH < WHITE_THRESH)
+    {
+        result = S_GREY;
+    }    
+    else
+        result = S_WHITE;
+    
+    return result;
+}
+
+void s_initialise()
+{
+    ADCSRA = BIT(ADEN);
 }
--- sensor.h
+++ sensor.h
@@ -9,13 +9,23 @@
  * License:
  *      GNU General Public License (see LICENSE in root folder)
  */
+#ifndef SENSOR_H
+#define SENSOR_H
 
+#include "config.h"
 #include "constants.h"
 
 #define S_WHITE ((unsigned char) 1)
 #define S_BLACK ((unsigned char) 2)
-#define S_GRAY  ((unsigned char) 3)
+#define S_GREY  ((unsigned char) 3)
 
 // Checks the given sensor and returns S_WHITE, S_BLACK or S_GRAY depending on
 // the result.
 unsigned char s_check(unsigned char sensor);
+
+/**
+initialises ADC ready for conversions
+*/
+void s_initialise(void);
+
+#endif
