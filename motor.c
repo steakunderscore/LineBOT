be1f1ebc02c9e1479d7f8e444948437779c9460a
Henry Jenkins <henry@henryjenkins.name>
2010-07-14 13:09:54 +1200

    Removed all PWM code and replaced it with GPIO code

--- motor.c
+++ motor.c
@@ -23,58 +23,8 @@ void m_initialize( void )
     pio_config_set (MOTOR_DRLEFT, PIO_OUTPUT);
     pio_config_set (MOTOR_DRRIGHT, PIO_OUTPUT);
     
-    /*
-       TCCR1 - Timer Counter Control Register (TIMER1)
-       -----------------------------------------------
-       BITS DESCRIPTION
-
-       TCCR1A
-       NO:   NAME   DESCRIPTION
-       --------------------------
-       BIT 0 : WGM10 [SET to 1]
-       BIT 1 : WGM11 [SET to 0]
-       BIT 2 : Don't care
-       BIT 3 : Don't care
-       BIT 4 : COM1B0 Invert selection           [SET to 0]
-       BIT 5 : COM1B1 Enable OC1B PWM            [SET to 1]
-       BIT 6 : COM1A0 Invert selection           [SET to 0]
-       BIT 7 : COM1A1 Enable OC1A PWM            [SET to 1]
-       
-       TCCR1B
-       NO:   NAME   DESCRIPTION
-       --------------------------
-       BIT 0 : CS10 [SET to 0]
-       BIT 1 : CS11 [SET to 1]
-       BIT 2 : CS12 [SET to 0]
-       BIT 3 : WGM12 [SET to 1]
-       BIT 4 : WGM13 [SET to 0]
-       Rest of bits we don't care about
-       
-       
-       The above settings are for
-       --------------------------
-       
-       Timer Clock = CPU Clock/8
-       Mode        = Fast PWM
-       PWM Output  = Non Inverted
-       Outputs     = OC1A and OC1B
-
-     */
-
-    TCCR1A = BIT(WGM10) | BIT(COM1B1) | BIT(COM1A1);
-    TCCR1B = BIT(CS11) | BIT(WGM12);
-
-    // Set OC1A and OC1B pins as the outputs.
-
-    //TODO: Im unsure about this line
-    DDRB|=(1<<PB1)|(1<<PB2);
-
-    //Set the output to off.
-    OCR1A=0x0000;
-    OCR1B=0x0000;
-
-    // Zero the counter
-    TCNT1 = 0;
+    pio_config_set (MOTOR_ENLEFT, PIO_OUTPUT);
+    pio_config_set (MOTOR_ENRIGHT, PIO_OUTPUT);
 }
 
 void m_turn(unsigned char direction) {
@@ -87,12 +37,12 @@ void m_turn(unsigned char direction) {
     
     switch (direction) {
         case LEFT:
-            OCR1A = 0x0000;
-            OCR1B = 0xFFFF;
+            pio_output_low (MOTOR_ENLEFT);
+            pio_output_high (MOTOR_ENRIGHT);
             break;
         case RIGHT:
-            OCR1A = 0xFFFF;
-            OCR1B = 0x0000;
+            pio_output_high (MOTOR_ENLEFT);
+            pio_output_low (MOTOR_ENRIGHT);
             break;  
         default:
             break;
@@ -114,8 +64,8 @@ void m_rotate(unsigned char direction) {
         pio_output_low (MOTOR_DRRIGHT);
     }
     // turn both the motors on
-    OCR1A = 0xFFFF;
-    OCR1B = 0xFFFF;
+    pio_output_high (MOTOR_ENLEFT);
+    pio_output_high (MOTOR_ENRIGHT);
 }
 
 void m_forwards( void ) {
@@ -124,8 +74,8 @@ void m_forwards( void ) {
     pio_output_low (MOTOR_DRRIGHT);
 
     // Adjust the motor speeds to full speed
-    OCR1A=0xFFFF;
-    OCR1B=0xFFFF;
+    pio_output_high (MOTOR_ENLEFT);
+    pio_output_high (MOTOR_ENRIGHT);
 }
 
 void m_forwardsD(unsigned char distance) {
@@ -145,8 +95,8 @@ void m_reverse( void ) {
     pio_output_low (MOTOR_DRRIGHT);
 
     // Adjust the motor speeds.
-    OCR1A=0xFFFF;
-    OCR1B=0xFFFF;
+    pio_output_high (MOTOR_ENLEFT);
+    pio_output_high (MOTOR_ENRIGHT);
 }
 
 void m_reverseD(unsigned char distance) {
@@ -162,19 +112,7 @@ void m_reverseD(unsigned char distance) {
 
 void m_stop( void ) {
     // Set PWM to be 0%
-    OCR1A=0x0000;
-    OCR1B=0x0000;
-}
-
-void m_control_motor(unsigned char motor, int speed) {
-    switch (motor) {
-        case LEFT:
-            OCR1A=speed;
-            break;
-        case RIGHT:
-            OCR1B=speed;
-            break;
-    }
-
+    pio_output_low (MOTOR_ENLEFT);
+    pio_output_low (MOTOR_ENRIGHT);
 }
 
