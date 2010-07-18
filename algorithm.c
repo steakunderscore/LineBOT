64e451e190c1c8ce420dbf20b2bed9f9c59f6fe9
Wim Looman <ghostunderscore@gmail.com>
2010-07-18 18:51:27 +1200

    Updating algorithm for 1MHz instead of 7.5MHz.

--- algorithm.c
+++ algorithm.c
@@ -18,7 +18,7 @@ void adjust( unsigned char direction1, unsigned char direction2, unsigned char s
 
     m_turn(direction1);
     while (s_check(sensor) != S_BLACK) {
-        _delay_ms(1.0);
+        _delay_ms(10.0);
         counter++;
     }
 
@@ -26,13 +26,33 @@ void adjust( unsigned char direction1, unsigned char direction2, unsigned char s
 
     m_turn(direction2);
     while (counter-- > 0) {
-        _delay_ms(1.0);
+        _delay_ms(10.0);
+    }
+}
+
+void turnRight( void ) {
+    m_stop();
+    _delay_ms(120.0);
+    if (s_check(CENTER) == S_WHITE && s_check(RIGHT) == S_WHITE) {
+        m_rotate(RIGHT);
+        _delay_ms(800.0);
+        m_stop();
+        if (s_check(CENTER) == S_WHITE && s_check(RIGHT) == S_WHITE) {
+          m_forwards();
+          _delay_ms(500.0);
+          m_rotate(RIGHT);
+          _delay_ms(800.0);
+          m_stop();
+        }
     }
 }
 
 void turnLeft( void ) {
+    m_stop();
+    _delay_ms(100.0);
     m_rotate(LEFT);
-    while (s_check(LEFT) != S_BLACK) ;
+    _delay_ms(800.0);
+    m_stop();
 }
 
 void goStraight( void ) {
@@ -41,19 +61,27 @@ void goStraight( void ) {
                   left   = S_WHITE;
 
     m_forwards();
+    _delay_ms(100.0);
     while (center == S_BLACK && right == S_BLACK && left == S_WHITE) {
         center = s_check(CENTER);
         right = s_check(RIGHT);
+        left = s_check(LEFT);
     }
+    center = s_check(CENTER);
+    right = s_check(RIGHT);
+    left = s_check(LEFT);
 
-    if (center == S_WHITE) {
+    if (left == S_BLACK) {
+        turnLeft();
+    }
+    else if (center == S_WHITE && right == S_WHITE) {
+        turnRight();
+    }
+    else if (center == S_WHITE) {
         adjust(RIGHT, LEFT, CENTER);
     }
     else if (right == S_WHITE) {
         adjust(LEFT, RIGHT, RIGHT);
-    }
-    else if (left == S_BLACK) {
-        turnLeft();
     }
 }
 
